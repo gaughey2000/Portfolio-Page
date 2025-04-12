1991e19c9fa5ce8925dd86e510baf4c7b1c6aa54
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-04-12 10:01:51 +0100

    3

--- my-website/my-website/src/Components/Navbar.jsx
+++ my-website/my-website/src/Components/Navbar.jsx
@@ -10,29 +10,24 @@ function Navbar () {
     return (
         <div className='w-screen h-16 bg-[#e0dac9] flex items-center justify-between px-4'>
             <div className='text-[#798a73] text-2xl font-bold'>
-                <h5>My Website</h5>
+                <h5>My Website</h5>               
             </div>
-            <div className='hidden md:flex space-x-4'>
-                <NavLink to="/" className='text-[#798a73] hover:text-[#798a73]'>Home</NavLink>
-                <NavLink to="/projects" className='text-[#798a73] hover:text-[#798a73]'>Projects</NavLink>
-                <NavLink to="/about" className='text-[#798a73] hover:text-[#798a73]'>About Me</NavLink>
-                <NavLink to="/contact" className='text-[#798a73] hover:text-[#798a73]'>Contact</NavLink>
+            <div className='flex md:hidden items-center hover:cursor-pointer'>
+                <box-icon onClick={toggleMenu} name='menu'></box-icon>
             </div>
-            <div className='md:hidden flex items-center'>
-                {/*<i onClick={toggleMenu} class='bx bx-menu' style='color:#798a73'  ></i>*/}
+            <div className={`flex flex-col md:flex-row items-center gap-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
+                <ul className={'gap-4 text-[#798a73]'}>
+                    <li>
+                        <NavLink to='/' className='hover:text-[#4b5c4a]'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about' className='hover:text-[#4b5c4a]'>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contact' className='hover:text-[#4b5c4a]'>Contact</NavLink>
+                    </li>
+                </ul>
             </div>
-            <div className={`absolute top-16 left-0 w-full bg-[#e0dac9] text-center transition-transform duration-300 ${isOpen ? 'transform translate-y-0' : 'transform -translate-y-full'}`}>
-                <div className='flex flex-col space-y-4 py-4'>
-                    <NavLink to="/" className='text-[#798a73] hover:text-[#798a73]'>Home</NavLink>
-                    <NavLink to="/projects" className='text-[#798a73] hover:text-[#798a73]'>Projects</NavLink>
-                    <NavLink to="/about" className='text-[#798a73] hover:text-[#798a73]'>About Me</NavLink>
-                    <NavLink to="/contact" className='text-[#798a73] hover:text-[#798a73]'>Contact</NavLink>
-                </div>
-                <div className='flex justify-center py-4'>
-                    <i onClick={toggleMenu} class='bx bx-x' style='color:#798a73' ></i>
-                </div>
-            </div>
-
         </div>
   );
 }
