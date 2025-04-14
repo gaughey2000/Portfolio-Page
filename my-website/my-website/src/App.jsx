5ef9483bfbe8c9c878eea572c1eb9204bdd67588
Connor McGaughey <cmcgaughey2000@icloud.com>
2025-04-14 13:12:15 +0100

    8

--- my-website/my-website/src/App.jsx
+++ my-website/my-website/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router";
+import About from './Pages/About'
 function App() {
 
   return (
-    <>
+    <div>
     <Navbar />
-    <Home />
-    </>
+    <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+    </div>
+
+    </div>
   )
 }
 
---
+++ my-website/my-website/src/Pages/About.jsx
@@ -0,0 +1,8 @@
+function About () {
+    return (
+        <div>
+            <h1 className="text-center text-2xl w-full top-0">About Me</h1>
+        </div>
+    )
+}
+export default About
